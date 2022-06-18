d109d5330f074ed7cf22a5da982860c237e3a0e7
Anna <taasonei@gmail.com>
2022-06-18 15:20:39 +0300

    Changed theme to no action bar. Enabled view binding. Added tab layout with two tabs recent and favourite and also added icons for these tabs

--- app/src/main/java/com/github/taasonei/MainActivity.kt
+++ app/src/main/java/com/github/taasonei/MainActivity.kt
@@ -1,11 +1,29 @@
 package com.github.taasonei
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.github.taasonei.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab?) {
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab?) {
+            }
+        })
     }
 }
