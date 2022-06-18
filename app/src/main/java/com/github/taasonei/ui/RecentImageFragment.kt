cef7f90430022ae62ca1be53e38b6635a8d98695
Anna <taasonei@gmail.com>
2022-06-18 19:42:08 +0300

    Created favourites ui. Created grid layout recycler view, added placeholder photo. File organization refactoring: added packages for model, ui and recycler files

--- app/src/main/java/com/github/taasonei/ui/RecentImageFragment.kt
+++ app/src/main/java/com/github/taasonei/ui/RecentImageFragment.kt
@@ -1,10 +1,11 @@
-package com.github.taasonei
+package com.github.taasonei.ui
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.github.taasonei.R
 
 class RecentImageFragment : Fragment() {
 
