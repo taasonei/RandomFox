7e41730ecc885968128eae52c0d559321689d646
Anna <taasonei@gmail.com>
2022-07-15 17:30:11 +0300

    Fix image cropping in recycler view

--- app/src/main/java/com/github/taasonei/randomfox/recycler/ImageCardViewHolder.kt
+++ app/src/main/java/com/github/taasonei/randomfox/recycler/ImageCardViewHolder.kt
@@ -2,7 +2,6 @@ package com.github.taasonei.randomfox.recycler
 
 import androidx.recyclerview.widget.RecyclerView
 import coil.load
-import coil.size.Scale
 import com.github.taasonei.randomfox.R
 import com.github.taasonei.randomfox.databinding.ImageCardItemBinding
 import com.github.taasonei.randomfox.model.FoxPhoto
@@ -13,7 +12,6 @@ class ImageCardViewHolder(private val binding: ImageCardItemBinding) :
         binding.likeCheckbox.isChecked = foxPhoto.isFavourite
         binding.foxPhoto.load(foxPhoto.image) {
             error(R.drawable.ic_baseline_broken_image)
-            scale(Scale.FILL)
         }
     }
 }
